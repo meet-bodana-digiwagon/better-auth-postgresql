8b7244d7a01fe0206120c3dfc4a0430c8e2771a2
Meet Bodana <meet.bodana@digiwagon.com>
2026-06-17 11:10:59 +0530

    chore: minnor fix

--- src/libraries/config/index.js
+++ src/libraries/config/index.js
@@ -4,10 +4,10 @@ import 'dotenv/config';
 const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
   NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
-  DATABASE_URL: z.string(),
+  DATABASE_URL: z.url(),
   BETTER_AUTH_SECRET: z.string().min(32),
-  BETTER_AUTH_URL: z.string().default('http://localhost:3000'),
-  CLIENT_ORIGIN_URL: z.string().default('http://localhost:5173'),
+  BETTER_AUTH_URL: z.url().default('http://localhost:3000'),
+  CLIENT_ORIGIN_URL: z.url().default('http://localhost:5173'),
 });
 
 export const env = envSchema.parse(process.env);
